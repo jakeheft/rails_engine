3f7ae42c4ee2a3fd42ba7c266d8d0406e2f39856
Jake Heft <jakeheft@gmail.com>
2020-12-14 16:37:38 -0700

    Add item show to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants
-      resources :items, only: %i[index]
+      resources :items, only: %i[index show]
     end
   end
 end
