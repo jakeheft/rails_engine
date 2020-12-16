b89452d547169280aaf61668a6ab7bfcee5ed9bc
Jake Heft <jakeheft@gmail.com>
2020-12-15 22:07:35 -0700

    Add route to search for item

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      get '/items/find', to: 'items#find'
       resources :merchants, except: %i[new edit]
       namespace :merchants do
         # resources :items, only: %i[index]
