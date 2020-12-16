26ce77a0ee98919062ba987a44046e3d6c30caec
Jake Heft <jakeheft@gmail.com>
2020-12-15 17:03:30 -0700

    Convert routes from only to except

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :merchants
-      resources :items, only: %i[index show create update]
+      resources :merchants, except: %i[new edit]
+      resources :items, except: %i[new edit]
     end
   end
 end
