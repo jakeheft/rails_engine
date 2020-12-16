f7a7439febff3a1da8af47895cd71f6326235113
Jake Heft <jakeheft@gmail.com>
2020-12-15 19:48:25 -0700

    Comment out trait that is not being used in merchant factory

--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -2,10 +2,10 @@ FactoryBot.define do
   factory :merchant do
     name { Faker::TvShows::SiliconValley.company }
 
-    trait :with_items do
-      after :create do |merchant|
-        create_list(:item, 2, merchant: merchant)
-      end
-    end
+    # trait :with_items do
+    #   after :create do |merchant|
+    #     create_list(:item, 2, merchant: merchant)
+    #   end
+    # end
   end
 end
