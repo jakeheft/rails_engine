4e6f8635591e2a8a596aba54c7e2a78036d13968
Jake Heft <jakeheft@gmail.com>
2020-12-15 18:38:12 -0700

    Add route for merchant items index

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, except: %i[new edit]
+      namespace :merchants do
+        # resources :items, only: %i[index]
+        get '/:id/items', to: 'items#index'
+      end
       resources :items, except: %i[new edit]
     end
   end
