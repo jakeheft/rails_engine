b4f766fb30d401482c94ce9eafa5308b1c8a66ac
Jake Heft <jakeheft@gmail.com>
2020-12-15 22:07:55 -0700

    Add controller find action to search for item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -22,6 +22,12 @@ class Api::V1::ItemsController < ApplicationController
     # render status: 204
   end
 
+  def find
+    attribute = params.keys.first
+    value = params.values.first
+    render json: ItemSerializer.new(Item.where("#{attribute} iLIKE '%#{value}%'").first)
+  end
+
   private
 
   def item_params
