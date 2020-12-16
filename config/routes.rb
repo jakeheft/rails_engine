4ffcb45d47b49c34fcee14bc810ac75f9fa6353a
Jake Heft <jakeheft@gmail.com>
2020-12-15 22:14:44 -0700

    Update find item route to be more restful

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      get '/items/find', to: 'items#find'
-      resources :merchants, except: %i[new edit]
       namespace :merchants do
-        # resources :items, only: %i[index]
         get '/:id/items', to: 'items#index'
       end
-      resources :items, except: %i[new edit]
+      resources :merchants, except: %i[new edit]
       namespace :items do
+        get '/find', to: 'find#show'
         get '/:id/merchants', to: 'merchants#index'
       end
+      resources :items, except: %i[new edit]
     end
   end
 end
