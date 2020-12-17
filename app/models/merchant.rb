ecad95064babfb5b9b0c7902dbe097f4588d0657
Jake Heft <jakeheft@gmail.com>
2020-12-16 19:39:51 -0700

    Add most items sold model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,12 @@
 class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :invoices
+
+  def self.most_revenue
+
+  end
+
+  def self.most_items_sold(quantity)
+    Merchant.joins(invoices: [:transactions, :invoice_items]).select('merchants.*, SUM(invoice_items.quantity) AS quantity').where('transactions.result = ?', 'success').group(:id).order('quantity DESC').limit(quantity)
+  end
 end
