68f37a8921531a3bef3d1db223a5dfb1a030a3bd
Jake Heft <jakeheft@gmail.com>
2020-12-16 21:29:11 -0700

    Add most revenue controller action

--- app/controllers/api/v1/business_intelligence_controller.rb
+++ app/controllers/api/v1/business_intelligence_controller.rb
@@ -1,12 +1,21 @@
 class Api::V1::BusinessIntelligenceController < ApplicationController
   def most_items_sold
     quantity = params[:quantity]
+    merchant_data = Merchant.most_items_sold(quantity)
     if quantity.to_i == 1
-      merchant = Merchant.most_items_sold(quantity)
-      render json: MerchantSerializer.new(merchant[0])
+      render json: MerchantSerializer.new(merchant_data[0])
     else
-      merchants = Merchant.most_items_sold(quantity)
-      render json: MerchantSerializer.new(merchants)
+      render json: MerchantSerializer.new(merchant_data)
+    end
+  end
+
+  def most_revenue
+    quantity = params[:quantity]
+    merchant_data = Merchant.most_revenue(quantity)
+    if quantity.to_i == 1
+      render json: MerchantSerializer.new(merchant_data[0])
+    else
+      render json: MerchantSerializer.new(merchant_data)
     end
   end
 end
