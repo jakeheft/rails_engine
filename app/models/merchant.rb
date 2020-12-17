508ac96989f9eabb2e9a222f5263762007d68ee5
Jake Heft <jakeheft@gmail.com>
2020-12-16 21:29:26 -0700

    Add most revenue merchant method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,11 +2,11 @@ class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :invoices
 
-  def self.most_revenue
-
+  def self.most_revenue(quantity)
+    Merchant.joins(invoices: %i[transactions invoice_items]).select('merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue').where('transactions.result = ?', 'success').where('invoices.status = ?', 'shipped').group(:id).order('revenue DESC').limit(quantity)
   end
 
   def self.most_items_sold(quantity)
-    Merchant.joins(invoices: [:transactions, :invoice_items]).select('merchants.*, SUM(invoice_items.quantity) AS quantity').where('transactions.result = ?', 'success').group(:id).order('quantity DESC').limit(quantity)
+    Merchant.joins(invoices: %i[transactions invoice_items]).select('merchants.*, SUM(invoice_items.quantity) AS quantity').where('transactions.result = ?', 'success').group(:id).order('quantity DESC').limit(quantity)
   end
 end
