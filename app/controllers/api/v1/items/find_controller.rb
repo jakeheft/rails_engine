ece37bc813be5a5b2265d19b923a7ecf3f17b0b3
Jake Heft <jakeheft@gmail.com>
2020-12-16 22:05:14 -0700

    Add find items index controller action

--- app/controllers/api/v1/items/find_controller.rb
+++ app/controllers/api/v1/items/find_controller.rb
@@ -2,6 +2,16 @@ class Api::V1::Items::FindController < ApplicationController
   def show
     attribute = params.keys.first
     value = params.values.first
-    render json: ItemSerializer.new(Item.where("#{attribute} iLIKE '%#{value}%'").first)
+    if attribute == 'created_at' || attribute == 'updated_at'
+      render json: ItemSerializer.new(Item.find_by())
+    else
+      render json: ItemSerializer.new(Item.find_by("#{attribute} iLIKE '%#{value}%'"))
+    end
+  end
+
+  def index
+    attribute = params.keys.first
+    value = params.values.first
+    render json: ItemSerializer.new(Item.where("#{attribute} iLIKE '%#{value}%'"))
   end
 end
