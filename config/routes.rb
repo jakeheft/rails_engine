4186a8fd72c3c87c722cfff7349c8777e46cceb7
Jake Heft <jakeheft@gmail.com>
2020-12-16 22:04:53 -0700

    Add route for search index for items

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
       resources :merchants, except: %i[new edit]
       namespace :items do
         get '/find', to: 'find#show'
+        get '/find_all', to: 'find#index'
         get '/:id/merchants', to: 'merchants#index'
       end
       resources :items, except: %i[new edit]
