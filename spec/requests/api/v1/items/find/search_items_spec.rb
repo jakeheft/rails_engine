30e309a2039b9edd13568e44e841125173d55a21
Jake Heft <jakeheft@gmail.com>
2020-12-16 22:04:37 -0700

    Add test for searching for multiple items

--- spec/requests/api/v1/items/find/search_items_spec.rb
+++ spec/requests/api/v1/items/find/search_items_spec.rb
@@ -34,19 +34,39 @@ describe 'Record search' do
     expect(description).to_not include('another')
   end
 
-  it 'can find a single item by description' do
-    create(:item)
+  it 'can find a single item by created_at' do
+    create(:item,)
     create(:item)
 
-    get '/api/v1/items/find?created_at=16'
+    get '/api/v1/items/find?created_at=2020_12_16T00:00:00 UTC'
 
     expect(response).to be_successful
 
     json = JSON.parse(response.body, symbolize_names: true)
     item = json[:data][:attributes]
     created_at = item[:created_at].downcase
-require "pry"; binding.pry
-    expect(created_at).to include('item')
-    expect(created_at).to_not include('another')
+# require "pry"; binding.pry
+    expect(created_at).to include('2020')
+    expect(created_at).to_not include('00:00')
+  end
+
+  it 'can find multiple items by name' do
+    create(:item, name: 'Fire Stone')
+    create(:item, name: 'Earth Stone')
+    create(:item, name: 'Chicken')
+
+    get '/api/v1/items/find_all?name=stone'
+
+    expect(response).to be_successful
+    json = JSON.parse(response.body, symbolize_names: true)
+    stones = json[:data]
+
+    expect(stones).to be_an(Array)
+    expect(stones.count).to eq(2)
+    stones.each do |stone|
+      expect(stone[:attributes][:name].downcase).to include('stone')
+      expect(stone[:attributes][:name].downcase).to_not include('chicken')
+      expect(stone).to be_a(Hash)
+    end
   end
 end
