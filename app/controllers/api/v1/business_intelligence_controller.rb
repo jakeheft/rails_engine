820b5319c5b10820301d27a71f0ed19e9250bd96
Jake Heft <jakeheft@gmail.com>
2020-12-16 19:39:24 -0700

    Move most items sold method to merchant model

--- app/controllers/api/v1/business_intelligence_controller.rb
+++ app/controllers/api/v1/business_intelligence_controller.rb
@@ -2,10 +2,10 @@ class Api::V1::BusinessIntelligenceController < ApplicationController
   def most_items_sold
     quantity = params[:quantity]
     if quantity.to_i == 1
-      merchant = MerchantFacade.most_items_sold(quantity)
+      merchant = Merchant.most_items_sold(quantity)
       render json: MerchantSerializer.new(merchant[0])
     else
-      merchants = MerchantFacade.most_items_sold(quantity)
+      merchants = Merchant.most_items_sold(quantity)
       render json: MerchantSerializer.new(merchants)
     end
   end
