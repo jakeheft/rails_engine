437ad44828c55912ea4c31e20bfc3c4b407a9846
Jake Heft <jakeheft@gmail.com>
2020-12-17 20:22:54 -0700

    Add revenue serializer to create json

---
+++ app/serializers/revenue_serializer.rb
@@ -0,0 +1,13 @@
+class RevenueSerializer
+  include FastJsonapi::ObjectSerializer
+  def self.revenue(data)
+    {
+  "data": {
+    "id": nil,
+    "attributes": {
+      "revenue": data[0][:revenue]
+    }
+  }
+}
+  end
+end
